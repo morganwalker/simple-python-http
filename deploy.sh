d6513330659e9ff084ea006a5a8d09a62b2248d9
Morgan Walker <morgan.walker@teachstone.com>
2017-10-19 21:46:53 -0400

    simplifying docker hub publish process

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'Docker Login - BEGIN'
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-echo 'Docker Login - END'
-
-echo 'Docker Push - BEGIN'
-docker push jmorganwalker/flask:$TRAVIS_TAG
-echo 'Docker Push - END'
